6417621ff0881b62af041d646415d60629edc36a
moeinerfanian <122752399+moeinerfanian@users.noreply.github.com>
2023-01-18 01:27:42 +0330

    Delete botdiscord directory

--- botdiscord/config.py
+++
@@ -1,7 +0,0 @@
-token = '' #Put Your bots token here
-
-prefix = '-' #put prefix here
-
-link = 'https://discord.gg/6AAukKWrV3' #put bot invite link here
-
-ownerid = '821006227633995797' #put your id here
